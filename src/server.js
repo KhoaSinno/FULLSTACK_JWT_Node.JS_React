691d1e57d5eff358f169ce0b73c14c1a1eb26268
Khoa Sinno <khoasinno@gmail.com>
2023-09-04 07:10:33 +0700

    #6.1 orm structure - p1

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 require('dotenv').config()
 import bodyParser from "body-parser";
+import connection from "./config/connectDB";
 
 const PORT = process.env.PORT || 8082
 const app = express() // create server
@@ -11,7 +12,12 @@ const app = express() // create server
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// check connect db
+connection()
+
+// config to use ejs
 configViewEngine(app)
+// route
 initWebRoutes(app)
 app.listen(PORT, () => {
     console.log(`<<< Run web server ${PORT}`)
