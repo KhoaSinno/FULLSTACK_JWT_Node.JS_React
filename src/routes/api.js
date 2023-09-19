967c29b397593917a10c6d504bbb9dc85e97b088
KhoaSN <khoasinno@gmail.com>
2023-09-19 18:50:55 +0700

    #10.1.3 Axios Post Data

---
+++ src/routes/api.js
@@ -0,0 +1,13 @@
+import express from "express";
+import apiController from "../controller/apiController"
+const router = express.Router()
+
+const initApiRoutes = (app) => {
+
+    router.get('/test-api', apiController.testApi)
+    router.post('/register', apiController.handleRegister)
+
+    return app.use("/api/v1/", router)
+}
+
+export default initApiRoutes
--- src/server.js
+++ src/server.js
@@ -1,31 +1,16 @@
 import express from "express";
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
+import initApiRoutes from "./routes/api";
+import configCors from "./config/cors"
 require('dotenv').config()
 import bodyParser from "body-parser";
 // import connection from "./config/connectDB";
 
 const PORT = process.env.PORT || 8082
 const app = express() // create server
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+// config cors
+configCors(app)
 
 //config body-parser to get data from client site (form frontend)
 app.use(bodyParser.json());
@@ -38,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 configViewEngine(app)
 // route
 initWebRoutes(app)
+initApiRoutes(app)
 app.listen(PORT, () => {
     console.log(`<<< Run web server ${PORT}`)
 })
