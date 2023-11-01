1d422c196666cd55aff59d21f61849a71cc74724
KhoaSN <khoasinno@gmail.com>
2023-11-01 09:24:54 +0700

    #10.6.5 Create New User - validdate

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,5 @@
 import userApiServices from "../service/userApiServices"
+import emailvalidator from 'email-validator'
 
 const readFunc = async (req, res) => {
     try {
@@ -30,11 +31,32 @@ const readFunc = async (req, res) => {
 const createFunc = async (req, res) => {
     try {
         //validate
+        if (!req.body.email || !req.body.phone || !req.body.password)
+            return res.status(200).json({
+                EM: 'Missing required parameter',
+                EC: 1,
+                DT: ''
+            })
+        if (!emailvalidator.validate(req.body.email)) {
+            return res.status(200).json({
+                EM: 'Email invalid',
+                EC: 1,
+                DT: ''
+            })
+        }
+
+        if (req.body.password && req.body.password.length < 4)
+            return res.status(200).json({
+                EM: 'Your password have to over 3 letters',
+                EC: -1,
+                DT: ''
+            })
+        //create user
         let data = await userApiServices.createUser(req.body)
         return res.status(200).json({
-            EM: data.EM, // Error message
-            EC: data.EC, // Error code
-            DT: data.DT // data
+            EM: data.EM,
+            EC: data.EC,
+            DT: data.DT
         })
     } catch (e) {
         console.log(e)
